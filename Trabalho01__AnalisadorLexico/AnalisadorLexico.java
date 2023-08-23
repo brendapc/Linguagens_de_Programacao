82a81527c73be3146ce16540e65e036de72832c1
Brenda Pereira <brendacamara@outlook.com>
2023-08-23 19:37:30 -0300

    tested code with input from terminal

--- Trabalho01__AnalisadorLexico/AnalisadorLexico.java
+++ Trabalho01__AnalisadorLexico/AnalisadorLexico.java
@@ -2,7 +2,7 @@ package Trabalho01__AnalisadorLexico;
 
 public class AnalisadorLexico {
     public static void main(String[] args) {
-        String text = args.length > 0 ? args[0] : "/* comentario teste */";
+        String text = args.length > 0 ? args[0] : "/* comentario 8888 ***** /////// 3131278172 teste */";
         if (text.startsWith("/*") && text.endsWith("*/"))
             System.out.println("Comentário válido!");
         else
